dd9f3d14fd2702e0f4eb21934a84629ce1f09999
Mukesh Rana <web@aativa.com>
2021-06-19 21:31:51 +0530

    fcm notification edit

--- Libs/FCMnotification.js
+++ Libs/FCMnotification.js
@@ -6,16 +6,16 @@ async function sendPushNotification(message1,deviceToken) {
     // return new Promise((resolve, reject) => {
         // try {
             // console.log("mesage", message1.message);
-            // var devicetoken = ["c-CRQPX_SaOU8IB6tj421i:APA91bEI8cGnHqPnr5APTn6f9_a4gqjc56qjsFiPXH9wlnTUc-IBnbvRIPWEu7gxBddSJmaks405nDTbNgN211gdGrND0dAC7gW1mXqUX1leDF9bOF09XIu_xqwyBULE8pzQnXGGuuog"]
+            var devicetoken = ["c-CRQPX_SaOU8IB6tj421i:APA91bEI8cGnHqPnr5APTn6f9_a4gqjc56qjsFiPXH9wlnTUc-IBnbvRIPWEu7gxBddSJmaks405nDTbNgN211gdGrND0dAC7gW1mXqUX1leDF9bOF09XIu_xqwyBULE8pzQnXGGuuog"]
             var message = {
-                registration_ids: deviceToken,
+                registration_ids: devicetoken,
                 // to:"dEeXtwXaQfyVynu2bpC0ri:APA91bFF9tOpjwwr_aMNeCqOlTU6ypMllqSlJ0IN6v130YxSF65_2Eivtquko-_a-TEGPBWK5H_CJvvYzytS3rsE4RjCr_SzILOFmcehcd_supwhBLR2xljeKRxY1BZwSTxIgIMPrInf",
-                notification: {
-                    title: message1.message.title,
-                    // message: "Hi there this is message",
-                    sound: 'default',
-                    badge: 1
-                },
+                // notification: {
+                //     title: message1.message.title,
+                //     // message: "Hi there this is message",
+                //     sound: 'default',
+                //     badge: 1
+                // },
                 data: message1.message,
 
                 priority: 'high'
@@ -23,11 +23,11 @@ async function sendPushNotification(message1,deviceToken) {
             fcm.send(message, function (err, result) {
                 console.log("hello");
                 if (err) {
-                    reject(err)
+                    // reject(err)
                     console.log("Something has gone wrong!", err);
                 } else {
                     console.log("Successfully sent with response: ", result);
-                    resolve(result)
+                    // resolve(result)
                 }
             });
         // }
